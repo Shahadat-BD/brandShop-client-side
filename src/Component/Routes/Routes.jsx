9e4415c3468f083fd36bec494a637d3bf6dc08f1
Shahadat-BD <shahadatstudent34@gmail.com>
2023-10-18 10:18:07 +0000

    react router setup and routing properly work

--- src/Component/Routes/Routes.jsx
+++ src/Component/Routes/Routes.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import Login from "../Authentication/Login/Login";
+import Register from "../Authentication/Register/Register";
+import Home from "../Pages/Home/Home";
+import Root from "../layout/Root";
+import NotFound from "../NotFound/NotFound";
+import AddProduct from "../Pages/AddProduct/AddProduct";
+import MyCart from "../Pages/MyCart/MyCart";
 
-const Routes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Root></Root>,
+        errorElement : <NotFound></NotFound>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            }, 
+            {
+                path: '/add-product',
+                element: <AddProduct></AddProduct>
+            }, 
+            {
+                path: '/my-cart',
+                element: <MyCart></MyCart>
+            }, 
+            {
+                path: '/login',
+                element: <Login></Login>
+            }, 
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
+        ]
+    }    
+]);
 
-export default Routes;
+export default router;
