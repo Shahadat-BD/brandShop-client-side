d982bca104d72c7ea692bb90dff13a51b77161ab
Shahadat-BD <shahadatstudent34@gmail.com>
2023-10-21 22:19:53 +0000

    project complete

--- src/Component/Pages/BrandProduct/BrandProduct.jsx
+++ src/Component/Pages/BrandProduct/BrandProduct.jsx
@@ -22,21 +22,21 @@ const BrandProduct = () => {
             advertise.map(advertiseImage => <>
                <div className="carousel w-full mb-10">
   <div id="slide1" className="carousel-item relative w-full">
-    <img src={advertiseImage.image1} className="w-full h-[450px]" />
+    <img src={advertiseImage.image1} className="w-full lg:h-[450px] md:h-[450px] h-[250px] rounded-md" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <img src={advertiseImage.image2} className="w-full h-[450px]" />
+    <img src={advertiseImage.image2} className="w-full lg:h-[450px] md:h-[450px] h-[250px] rounded-md" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" className="btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-    <img src={advertiseImage.image3} className="w-full h-[450px]" />
+    <img src={advertiseImage.image3} className="w-full lg:h-[450px] md:h-[450px] h-[250px] rounded-md" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" className="btn btn-circle">❮</a> 
       <a href="#slide4" className="btn btn-circle">❯</a>
